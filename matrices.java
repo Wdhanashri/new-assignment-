b4f2a7954e15a313523db4268f49606102a86504
Wdhanashri <dhanashriwankhade212@gmail.com>
2025-01-31 23:31:29 +0530

    Create matrices.java

---
+++ matrices.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input dimensions for matrices
+        System.out.print("Enter number of rows for first matrix: ");
+        int rows1 = sc.nextInt();
+        System.out.print("Enter number of columns for first matrix (also rows for second matrix): ");
+        int cols1 = sc.nextInt();
+        System.out.print("Enter number of columns for second matrix: ");
+        int cols2 = sc.nextInt();
+
+        int[][] matrix1 = new int[rows1][cols1];
+        int[][] matrix2 = new int[cols1][cols2];
+        int[][] result = new int[rows1][cols2];
+
+        
+        System.out.println("Enter elements of first matrix:");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols1; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+
+      
+        System.out.println("Enter elements of second matrix:");
+        for (int i = 0; i < cols1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+    
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                for (int k = 0; k < cols1; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+    
+        System.out.println("Resultant Matrix after Multiplication:");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
